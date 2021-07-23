f988224f42cf9094966e8515949b2794b02dc78b
shahzadkhan1998 <sk266349@gmail.com>
2021-07-24 00:40:27 +0500

    Comment you want

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_practice/pages/home_page.dart';
 
 void main() {
   runApp(Myapp());
@@ -10,13 +11,7 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("This is my First long Practice"),
-          ),
-        ),
-      ),
+      home: Home(),
     );
   }
 }
